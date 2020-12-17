57a9a2a5f57c4ff4ad869e30b31e641e45b2b6a8
Vladislav Rastorguev <4etkuu-poz@mail.ru>
2020-12-17 13:01:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ class Requests {
             let data = await fetch('http://127.0.0.1:3000/' + url, {
                 credentials: 'include',
                 headers: {
-                    cookie: 'token=kek'
+                    cookies: 'token=kek'
                 }
             });
             return await data.json();
